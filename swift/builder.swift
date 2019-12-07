7a486cbe638fd47eb4fcdd8aee8ab1c37567997e
Max Bruckner <max@maxbruckner.de>
2019-12-07 22:26:14 +0100

    Swift: Use protocols and structs instead of classes!

--- swift/builder.swift
+++ swift/builder.swift
@@ -1,14 +1,16 @@
-class StaticOptional<Type> {}
-
-class StaticNone<Type> : StaticOptional<Type> {}
-class StaticSome<Type>: StaticOptional<Type> {
-	let element: Type
-
-	init(element: Type) {
-		self.element = element
-	}
+protocol StaticOptional {
+	associatedtype Element
 }
 
+struct StaticSome<Type>: StaticOptional {
+	typealias Element = Type
+
+	let element: Type
+}
+
+struct StaticNone<Type>: StaticOptional {
+	typealias Element = Type
+}
 
 struct Point3D: Equatable {
 	let x: Double
@@ -18,24 +20,24 @@ struct Point3D: Equatable {
 
 struct Point3DBuilder<X, Y, Z>
 	where
-		X: StaticOptional<Double>,
-		Y: StaticOptional<Double>,
-		Z: StaticOptional<Double>
+		X: StaticOptional,
+		X.Element == Double,
+		Y: StaticOptional,
+		Y.Element == Double,
+		Z: StaticOptional,
+		Z.Element == Double
 {
 	let x: X
 	let y: Y
 	let z: Z
 }
 
-func point3d() -> Point3DBuilder<StaticNone<Double>, StaticNone<Double>, StaticNone<Double>> {
+func point3d() -> Point3DBuilder<StaticNone<Double>,StaticNone<Double>,StaticNone<Double>> {
 	return Point3DBuilder(x: StaticNone(), y: StaticNone(), z: StaticNone())
 }
 
 extension Point3DBuilder
-	where
-		X: StaticNone<Double>,
-		Y: StaticOptional<Double>,
-		Z: StaticOptional<Double>
+	where X == StaticNone<Double>
 {
 	func x(_ x: Double) -> Point3DBuilder<StaticSome<Double>, Y, Z> {
 		return Point3DBuilder<StaticSome<Double>, Y, Z>(x: StaticSome(element: x), y: self.y, z: self.z)
@@ -43,10 +45,7 @@ extension Point3DBuilder
 }
 
 extension Point3DBuilder
-	where
-		X: StaticOptional<Double>,
-		Y: StaticNone<Double>,
-		Z: StaticOptional<Double>
+	where Y == StaticNone<Double>
 {
 	func y(_ y: Double) -> Point3DBuilder<X, StaticSome<Double>, Z> {
 		return Point3DBuilder<X, StaticSome<Double>, Z>(x: self.x, y: StaticSome(element: y), z: self.z)
@@ -54,10 +53,7 @@ extension Point3DBuilder
 }
 
 extension Point3DBuilder
-	where
-		X: StaticOptional<Double>,
-		Y: StaticOptional<Double>,
-		Z: StaticNone<Double>
+	where Z == StaticNone<Double>
 {
 	func z(_ z: Double) -> Point3DBuilder<X, Y, StaticSome<Double>> {
 		return Point3DBuilder<X, Y, StaticSome<Double>>(x: self.x, y: self.y, z: StaticSome(element: z))
@@ -66,9 +62,9 @@ extension Point3DBuilder
 
 extension Point3DBuilder
 	where
-		X: StaticSome<Double>,
-		Y: StaticSome<Double>,
-		Z: StaticSome<Double>
+		X == StaticSome<Double>,
+		Y == StaticSome<Double>,
+		Z == StaticSome<Double>
 {
 	func build() -> Point3D {
 		return Point3D(x: self.x.element, y: self.y.element, z: self.z.element)
